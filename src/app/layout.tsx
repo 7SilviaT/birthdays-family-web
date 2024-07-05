4c4344b67c7055978b290732b9c0663ff0ae370e
7SilviaT <eagles8485@gmail.com>
2024-07-04 21:55:08 -0500

    feat: initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
+
+export const metadata: Metadata = {
+  title: "Birthdays",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} bg-[#C0E0EA] container p-10 `}>{children}</body>
+    </html>
+  );
+}
